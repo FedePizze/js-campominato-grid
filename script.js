878d8402f67111da93a7d19aefe7ddb94977423f
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-02-01 17:53:12 +0100

    aggiunto conta click

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const griglia = document.getElementById('cont2');
 const regole = document.getElementById('regole');
+const fine = document.getElementById('fine');
 
 const play = document.getElementById('play');
 const select = document.getElementById('select');
@@ -8,6 +9,9 @@ const select = document.getElementById('select');
 
 play.addEventListener('click', function() {
     griglia.innerHTML = '';
+    let click = 0;
+
+    
 
     if (select.value == 'Insane') {
         var numeroCaselle = 49;
@@ -30,26 +34,37 @@ play.addEventListener('click', function() {
         node.classList.add(i)
         node.classList.add('aaa')
 
-        node.addEventListener('click', function() 
-            {
-                this.classList.add('squareBG');
-            })
+        node.addEventListener('click', clickStatus);
+            
+
+        function clickStatus() {
+            this.classList.add('squareBG');
+            this.removeEventListener('click', clickStatus)
+            click = click + 1;
+            console.log(click)
+        }
 
         griglia.appendChild(node);
     }
 
     for (let i = 0; i <= 16; i++) {
         let elements = document.querySelectorAll('.aaa')
-        let random1 = Math.floor(Math.random() * elements.length);
+        let random = Math.floor(Math.random() * elements.length);
+        //console.log(random)
         
-        elements[random1].addEventListener('click', function() 
+        elements[random].addEventListener('click', function() 
             {
                 this.classList.add('squareBG2');
+                fine.innerHTML = 'hai perso dopo ' + click + ' mosse';
                 setTimeout(() => {alert('BOOM! Hai beccato una mina')}, 200);
                 setTimeout(() => {griglia.innerHTML = ''}, 1000);
+                setTimeout(() => {fine.innerHTML = ''}, 1000);
             })
     }
+
+    
 })
+
 
 
 
